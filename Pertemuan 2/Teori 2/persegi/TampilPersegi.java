26f354cbb479f2fb39a190ca7eb20290c44bada7
Risky Dwi Ramadhan <riskydwiramadhan956@gmail.com>
2020-09-09 15:20:07 +0700

    Teori 2

--- Pertemuan 2/Teori 2/persegi/TampilPersegi.java
+++ Pertemuan 2/Teori 2/persegi/TampilPersegi.java
@@ -9,8 +9,20 @@ import java.util.*;
 /** @pdOid 7056a9eb-61a3-40f2-8677-05901d6cc7b0 */
 public class TampilPersegi {
    /** @pdOid c80439b0-0530-4dfa-88e4-a4a3fd7ac90e */
-   public static void main() {
+   public static void tampilSisi() {
       // TODO: implement
+   }
+   
+   /** @pdOid 8d82e250-f3d4-420b-ab76-8ee92d05d8ad */
+   public int tampilLuas() {
+      // TODO: implement
+      return 0;
+   }
+   
+   /** @pdOid 1f5f98f0-20df-4dbf-b072-198dfd2289eb */
+   public int tampilKel() {
+      // TODO: implement
+      return 0;
    }
 
 }
